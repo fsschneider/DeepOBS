e598434fee752a0bbff31165777df0d79350b378
abahde <aaron.bahde@student.uni-tuebingen.de>
2019-05-14 17:08:57 +0200

    implemented tensorboard logging to the abstraction of runners

--- deepobs/tensorflow/runners/runner.py
+++ deepobs/tensorflow/runners/runner.py
@@ -30,7 +30,8 @@ class TFRunner(Runner, abc.ABC):
 
         # TODO sketch the structure of a runner
         """..."""
-        run_folder_name, file_name = self.create_output_directory(testproblem,
+
+        self._run_folder_name, file_name = self.create_output_directory(testproblem,
                                      num_epochs,
                                      batch_size,
                                      weight_decay,
@@ -58,7 +59,7 @@ class TFRunner(Runner, abc.ABC):
                   **output}
 
         if not no_logs:
-            self.write_output(output, run_folder_name, file_name)
+            self.write_output(output, self._run_folder_name, file_name)
 
     @staticmethod
     def create_testproblem(testproblem, batch_size, weight_decay, random_seed):
@@ -83,7 +84,7 @@ class TFRunner(Runner, abc.ABC):
     # Wrapper functions for the evaluation phase.
     # TODO get rid of test arg and split into two function for clarity?
     @staticmethod
-    def evaluate(tproblem, sess, loss, tf_logging, test=True):
+    def evaluate(tproblem, sess, loss, test=True):
         """Computes average loss and accuracy in the evaluation phase."""
         if test:
             sess.run(tproblem.test_init_op)
@@ -116,26 +117,10 @@ class TFRunner(Runner, abc.ABC):
                     break
         loss_ /= num_iters
         acc_ /= num_iters
-
-        # TODO how to abstract the tf logging
-#        # Log results to tensorflow summaries
-#        if tf_logging:
-#            if test:
-#                tag = "epoch/test_"
-#            else:
-#                tag = "epoch/train_"
-#            summary = tf.Summary()
-#            summary.value.add(tag=tag + "loss_", simple_value=loss_)
-#            summary.value.add(tag=tag + "acc_", simple_value=acc_)
-#            per_epoch_summary_ = sess.run(per_epoch_summaries)
-#            summary_writer.add_summary(per_epoch_summary_,
-#                                       len(loss_list) - 1)
-#            summary_writer.add_summary(summary, len(loss_list) - 1)
-#            summary_writer.flush()
 #
         print("{0:s} loss {1:g}, acc {2:f}".format(msg, loss_, acc_))
 
-        # Print and log the results.
+        # Print and return the results.
         return loss_, acc_
 
 
@@ -145,62 +130,65 @@ class TFRunner(Runner, abc.ABC):
         return
 
 class TFStandardRunner(TFRunner):
-    """Provides functionality to run optimizers on DeepOBS testproblems including
-    the logging of important performance metrics.
 
-    Args:
-      optimizer_class: Optimizer class, which should inherit from
-          tf.train.Optimizer and/or obey the same interface for ``.minimize()``.
-      hyperparams: A list describing the optimizer's hyperparameters other
-          than learning rate. Each entry of the list is a dictionary describing
-          one of the hyperparameters. This dictionary is expected to have the
-          following two fields:
-
-            - hyperparams["name"] must contain the name of the parameter (i.e.,
-              the exact name of the corresponding keyword argument to the
-              optimizer class' init function.
-            - hyperparams["type"] specifies the type of the parameter (e.g.,
-              ``int``, ``float``, ``bool``).
-
-          Optionally, the dictionary can have a third field indexed by the key
-          "default", which specifies a default value for the hyperparameter.
-
-    Example
-    --------
-    >>> optimizer_class = tf.train.MomentumOptimizer
-    >>> hyperparams = [
-            {"name": "momentum", "type": float},
-            {"name": "use_nesterov", "type": bool, "default": False}]
-    >>> runner = StandardRunner(optimizer_class, hyperparms)
-
-    """
 
     def __init__(self, optimizer_class, hyperparams):
-        """Creates a new StandardRunner.
 
-    Args:
-      optimizer_class: Optimizer class, which should inherit from
-          tf.train.Optimizer and/or obey the same interface for ``.minimize()``.
-      hyperparams: A list describing the optimizer's hyperparameters other
-          than learning rate. Each entry of the list is a dictionary describing
-          one of the hyperparameters. This dictionary is expected to have the
-          following two fields:
-            - hyperparams["name"] must contain the name of the parameter (i.e.,
-              the exact name of the corresponding keyword argument to the
-              optimizer class' init function.
-            - hyperparams["type"] specifies the type of the parameter (e.g.,
-              ``int``, ``float``, ``bool``).
-          Optionally, the dictionary can have a third field indexed by the key
-          "default", which specifies a default value for the hyperparameter.
-
-    Example:
-        optimizer_class = tf.train.MomentumOptimizer
-        hyperparams = [
-            {"name": "momentum", "type": float},
-            {"name": "use_nesterov", "type": bool, "default": False}]
-        runner = StandardRunner(optimizer_class, hyperparms)
-    """
         super(TFStandardRunner, self).__init__(optimizer_class, hyperparams)
+
+    def init_summary(self, loss,
+                     learning_rate_var,
+                     batch_size,
+                     log_dir):
+        # per iteration
+        mb_train_loss_summary = tf.summary.scalar(
+            "training/minibatch_train_losses",
+            loss,
+            collections=[tf.GraphKeys.SUMMARIES, "per_iteration"])
+        # per epoch
+        lr_summary = tf.summary.scalar(
+            "hyperparams/learning_rate",
+            learning_rate_var,
+            collections=[tf.GraphKeys.SUMMARIES, "per_epoch"])
+        batch_summary = tf.summary.scalar(
+            "hyperparams/batch_size",
+            batch_size,
+            collections=[tf.GraphKeys.SUMMARIES, "per_epoch"])
+
+        per_iter_summaries = tf.summary.merge_all(key="per_iteration")
+        per_epoch_summaries = tf.summary.merge_all(key="per_epoch")
+        summary_writer = tf.summary.FileWriter(log_dir)
+        return per_iter_summaries, per_epoch_summaries, summary_writer
+
+    def write_per_epoch_summary(self,
+                                sess,
+                                loss_,
+                                acc_,
+                                current_step,
+                                per_epoch_summaries,
+                                summary_writer,
+                                test=True):
+        if test:
+            tag = "epoch/test_"
+        else:
+            tag = "epoch/train_"
+        summary = tf.Summary()
+        summary.value.add(tag=tag + "loss_", simple_value=loss_)
+        summary.value.add(tag=tag + "acc_", simple_value=acc_)
+        per_epoch_summary_ = sess.run(per_epoch_summaries)
+        summary_writer.add_summary(per_epoch_summary_,
+                                   current_step)
+        summary_writer.add_summary(summary, current_step)
+        summary_writer.flush()
+        return
+
+    def write_per_iter_summary(self,
+                               sess,
+                               per_iter_summaries,
+                               summary_writer,
+                               current_step):
+        per_iter_summary_ = sess.run(per_iter_summaries)
+        summary_writer.add_summary(per_iter_summary_, current_step)
 
     # This function is a wrapper around _run() which grabs all non-specified
     # arguments from the command line.
@@ -210,9 +198,11 @@ class TFStandardRunner(TFRunner):
             lr_sched_epochs=None,
             lr_sched_factors=None,
             train_log_interval=10,
-            print_train_iter=None,
-            tf_logging=None):
+            print_train_iter=False,
+            tf_logging=False,
+            tf_logging_dir='./tensorboard_logs'):
 
+        # TODO abstract loss
         loss = tf.reduce_mean(tproblem.losses) + tproblem.regularizer
 
         # Set up the optimizer and create learning rate schedule.
@@ -244,27 +234,12 @@ class TFStandardRunner(TFRunner):
         test_accuracies = []
 
         # Tensorboard summaries
-        # TODO how to abstract the tf logging
-#        if tf_logging:
-#            # per iteration
-#            mb_train_loss_summary = tf.summary.scalar(
-#                "training/minibatch_train_losses",
-#                loss,
-#                collections=[tf.GraphKeys.SUMMARIES, "per_iteration"])
-#            # per epoch
-#            lr_summary = tf.summary.scalar(
-#                "hyperparams/learning_rate",
-#                learning_rate_var,
-#                collections=[tf.GraphKeys.SUMMARIES, "per_epoch"])
-#            batch_summary = tf.summary.scalar(
-#                "hyperparams/batch_size",
-#                batch_size,
-#                collections=[tf.GraphKeys.SUMMARIES, "per_epoch"])
-#
-#            per_iter_summaries = tf.summary.merge_all(key="per_iteration")
-#            per_epoch_summaries = tf.summary.merge_all(key="per_epoch")
-#            summary_writer = tf.summary.FileWriter(directory)
-
+        if tf_logging:
+            batch_size = tproblem._batch_size
+            per_iter_summaries, per_epoch_summaries, summary_writer = self.init_summary(loss,
+                                                                                        learning_rate_var,
+                                                                                        batch_size,
+                                                                                        tf_logging_dir)
         # Start tensorflow session and initialize variables.
         sess = tf.Session()
         sess.run(tf.global_variables_initializer())
@@ -274,8 +249,32 @@ class TFStandardRunner(TFRunner):
             # Evaluate at beginning of epoch.
             print("********************************")
             print("Evaluating after {0:d} of {1:d} epochs...".format(n, num_epochs))
-            self.evaluate(tproblem, sess, loss, tf_logging, test=False)
-            self.evaluate(tproblem, sess, loss, tf_logging, test=True)
+
+            loss_, acc_ = self.evaluate(tproblem, sess, loss, test=False)
+            if tf_logging:
+                current_step = len(train_losses)
+                self.write_per_epoch_summary(sess,
+                                         loss_,
+                                         acc_,
+                                         current_step,
+                                         per_epoch_summaries,
+                                         summary_writer,
+                                         test=False)
+            train_losses.append(loss_)
+            train_accuracies.append(acc_)
+
+            loss_, acc_ = self.evaluate(tproblem, sess, loss, test=True)
+            if tf_logging:
+                current_step = len(test_losses)
+                self.write_per_epoch_summary(sess,
+                                         loss_,
+                                         acc_,
+                                         current_step,
+                                         per_epoch_summaries,
+                                         summary_writer,
+                                         test=True)
+            test_losses.append(loss_)
+            test_accuracies.append(acc_)
             print("********************************")
 
             # Break from train loop after the last round of evaluation
@@ -290,16 +289,18 @@ class TFStandardRunner(TFRunner):
             s = 0
             while True:
                 try:
-                    # Training step, with logging if we hit the train_log_interval
                     if s % train_log_interval == 0:
-                        # TODO how to abstract the logging?
-#                        if tf_logging:
-#                            _, loss_, per_iter_summary_ = sess.run(
-#                                [step, loss, per_iter_summaries])
-#                            summary_writer.add_summary(per_iter_summary_,
-#                                                       sess.run(global_step))
-#                        else:
+                        # Training step, with logging if we hit the train_log_interval
                         _, loss_ = sess.run([step, loss])
+                        minibatch_train_losses.append(loss_.astype(float))
+
+                        if tf_logging:
+                            current_step = sess.run(global_step)
+                            self.write_per_iter_summary(sess,
+                                                        per_iter_summaries,
+                                                        summary_writer,
+                                                        current_step)
+
                         minibatch_train_losses.append(loss_.astype(float))
                         if print_train_iter:
                             print("Epoch {0:d}, step {1:d}: loss {2:g}".format(
