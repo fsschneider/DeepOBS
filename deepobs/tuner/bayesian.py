e50c1a9ff3e7b6949e0e8bdd48fe004e5611bdea
abahde <aaron.bahde@student.uni-tuebingen.de>
2019-06-11 16:11:01 +0200

    added GP

--- deepobs/tuner/bayesian.py
+++ deepobs/tuner/bayesian.py
@@ -1,22 +1,49 @@
 # -*- coding: utf-8 -*-
 from .tuner import Tuner
-from .. import config
-class RandomSearch(Tuner):
+from GPyOpt.methods import BayesianOptimization as BO
+class GP(Tuner):
+    def __init__(self, optimizer_class,
+                 ressources,
+                 bounds,
+                 acquisition_function = 'EI',
+                 runner_type='StandardRunner'):
 
-    def __init__(self, optimizer_class, hyperparams, ressources, distributions, mode = 'final', testproblems=None, runner_type='StandardRunner'):
-        super(RandomSearch).__init__(self, optimizer_class, hyperparams, ressources, mode, testproblems, runner_type)
+# TODO if the same tuning run is done again but with e.g. different acquisition function the results folder is already filled with previous runs!
+        # make sure to use another folder in this case
+        hyperparams = [dic['name'] for dic in bounds]
+        super(GP, self).__init__(optimizer_class, hyperparams, ressources, runner_type)
 
-        self._distributions = distributions
-    def tune(self, testproblem, **training_params):
-        # TODO parallelize
-        # TODO return the commands if no parallelization possible
-        for i in range(self._ressources):
-            # sample parameters
-            params = {}
-            for param_name, param_distr in self._distributions.iteritems():
-                params[param_name] = param_distr.rvs()
+        self._acquisition_function = acquisition_function
+        self._bounds = bounds
 
-            runner = self._runner(self._optimizer_class, params)
-            result = runner.run(testproblem, **config.get_testproblem_default_setting(testproblem), **training_params)
-        return
+    def _build_hyperparams_dict_from_proposal(self, proposal_array):
+        hyperparams = {}
+        for index, key in enumerate(self._hyperparams):
+            # TODO why the workaround with the extra dimension of the proposal?
+            hyperparams[key] = proposal_array[0][index]
+        return hyperparams
 
+    def _generate_cost_function(self, testproblem, **kwargs):
+        '''Factory to create the cost function depending on the testproblem'''
+        def _cost_function(proposal_array):
+            hyperparams = self._build_hyperparams_dict_from_proposal(proposal_array)
+            runner = self._runner(self._optimizer_class)
+            output = runner.run(testproblem, hyperparams, **kwargs)
+            # TODO which mode to use here? This is final.
+            # TODO which metric to use? if available...
+            # TODO attention its -1 since the method looks for minimum
+            cost = -1*output['test_accuracies'][-1]
+            return cost
+        return _cost_function
+
+    def tune(self, testproblem, **kwargs):
+        # TODO how to use different kernels
+        # TODO what about categricals?
+        cost_function = self._generate_cost_function(testproblem, **kwargs)
+        # TODO how to deal with minimum amount of init samples?
+        op = BO(f=cost_function, domain=self._bounds, acquisition_type=self._acquisition_function, initial_design_numdata=5)
+        op.run_optimization(max_iter=self._ressources)
+        return op
+
+class TPE(Tuner):
+    pass
