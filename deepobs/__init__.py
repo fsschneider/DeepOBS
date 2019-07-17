5d6bc5bafea80b1d005aac3bc92f4704d822af23
abahde <aaron.bahde@student.uni-tuebingen.de>
2019-07-17 19:04:02 +0200

    removed frameworks from init. so the framework is only imported if the framework are accessed

--- deepobs/__init__.py
+++ deepobs/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from . import tensorflow
+#from . import tensorflow
 from . import analyzer
 from . import scripts
-from . import pytorch
+#from . import pytorch
 from . import config
 from . import tuner
 from . import abstract_runner
