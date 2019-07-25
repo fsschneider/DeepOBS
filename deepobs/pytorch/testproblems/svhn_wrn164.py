92fe47e648a013f99036d87a957b8fe2b5db6b17
abahde <aaron.bahde@student.uni-tuebingen.de>
2019-07-25 17:22:43 +0200

    added proper docs

--- deepobs/pytorch/testproblems/svhn_wrn164.py
+++ deepobs/pytorch/testproblems/svhn_wrn164.py
@@ -4,12 +4,42 @@ from .testproblems_modules import net_wrn
 from ..datasets.svhn import svhn
 from .testproblem import TestProblem
 
+
 class svhn_wrn164(TestProblem):
+    """DeepOBS test problem class for the Wide Residual Network 16-4 architecture\
+    for SVHN.
+
+  Details about the architecture can be found in the `original paper`_.
+  A weight decay is used on the weights (but not the biases)
+  which defaults to ``5e-4``.
+
+  Training settings recommenden in the `original paper`_:
+  ``batch size = 128``, ``num_epochs = 160`` using the Momentum optimizer
+  with :math:`\\mu = 0.9` and an initial learning rate of ``0.01`` with a decrease by
+  ``0.1`` after ``80`` and ``120`` epochs.
+
+  .. _original paper: https://arxiv.org/abs/1605.07146
+
+  Args:
+    batch_size (int): Batch size to use.
+    weight_decay (float): Weight decay factor. Weight decay (L2-regularization)
+        is used on the weights but not the biases.
+        Defaults to ``5e-4``.
+  """
+
     def __init__(self, batch_size, weight_decay=0.0005):
+        """Create a new WRN 16-4 test problem instance on SVHN.
+
+        Args:
+          batch_size (int): Batch size to use.
+          weight_decay (float): Weight decay factor. Weight decay (L2-regularization)
+              is used on the weights but not the biases.
+              Defaults to ``5e-4``.
+        """
         super(svhn_wrn164, self).__init__(batch_size, weight_decay)
 
     def set_up(self):
-        """Set up the vanilla CNN test problem on Cifar-10."""
+        """Set up the Wide ResNet 16-4 test problem on SVHN."""
         self.data = svhn(self._batch_size, data_augmentation=True)
         self.loss_function = nn.CrossEntropyLoss()
         self.net = net_wrn(num_outputs=10, num_residual_blocks=2, widening_factor=4)
---
+++ docs/api/pytorch/testproblems/svhn/svhn_wrn164.rst
@@ -0,0 +1,8 @@
+=================
+SVHN Wide Resnet
+=================
+
+.. currentmodule:: deepobs.pytorch.testproblems.svhn_wrn164
+
+.. autoclass:: svhn_wrn164
+    :members:
