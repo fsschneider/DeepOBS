fe9d13c3d6eb7755852d14bb58c95c1801f222c2
abahde <aaron.bahde@student.uni-tuebingen.de>
2019-07-28 19:33:35 +0200

    wrote docs for gp tuner

--- deepobs/tuner/bayesian.py
+++ deepobs/tuner/bayesian.py
@@ -8,11 +8,11 @@ from .tuner_utils import rerun_setting
 
 
 class GP(Tuner):
-    # TODO write docstrings
     """A Bayesian optimization tuner that uses a Gaussian Process surrogate.
     Attributes:
          _bounds (dict): The bounds for each hyperparameter.
     Methods:
+        _convert_categoricals_for_cost_function
     """
     def __init__(self, optimizer_class,
                  hyperparam_names,
@@ -40,12 +40,20 @@ class GP(Tuner):
             raise NotImplementedError('''Mode not available for this tuning method. Please use final or best.''')
         return cost
 
-    def _convert_categoricals_for_cost_function(self, **hyperparams):
-        """Overwrite this method if you want to read in categoricals in a different way, e.g. a mapping from integers to strings."""
-        for key, value in hyperparams.items():
+    def _convert_categoricals_for_cost_function(self, **hyperparams_sample):
+        """This function edits a new proposed sample of hyperparameters before they are parsed to the runner. This way,
+        we can convert continous samples to discretes. Overwrite this method if you want to read in categoricals in a
+        different way, e.g. a mapping from integers to strings.
+        Args:
+            hyperparams_sample (dict): The sample of the optimizer hyperparams which is to evaluate next.
+        Returns:
+            dict: The transformed hyperparams sample that is evaluated next.
+            """
+
+        for key, value in hyperparams_sample.items():
             if self._hyperparam_names[key]['type'] == bool or self._hyperparam_names[key]['type'] == int:
-                hyperparams[key] = round(value)
-        return hyperparams
+                hyperparams_sample[key] = round(value)
+        return hyperparams_sample
 
     def _generate_cost_function(self, testproblem, output_dir, mode, **kwargs):
         '''Factory to create the cost function depending on the testproblem and kwargs.'''
@@ -85,13 +93,28 @@ class GP(Tuner):
              tuning_summary = True, 
              plotting_summary = True, 
              kernel = None,
-             alpha = None,
              acq_type = 'ucb', 
              acq_kappa = 2.576, 
              acq_xi = 0.0, 
              mode = 'final',
              rerun_best_setting = False,
              **kwargs):
+
+        """Tunes the optimizer hyperparameters by evaluating a Gaussian process surrogate with an acquisition function.
+        Args:
+            testproblem (str): The test problem to tune the optimizer on.
+            output_dir (str): The output directory for the results.
+            random_seed (int): Random seed for the whole truning process. Every individual run is seeded by it.
+            n_init_samples (int): The number of random exploration samples in the beginning of the tuning process.
+            tuning_summary (bool): Whether to write an additional tuning summary. Can be used to get an overview over the tuning progress
+            plotting_summary (bool): Whether to store additional objects that can be used to plot the posterior.
+            kernel (Sklearn.gaussian_process.kernels.Kernel): The kernel of the GP.
+            acq_type (str): The type of acquisition function to use. Muste be one of ``ucb``, ``ei``, ``poi``.
+            acq_kappa (float): Scaling parameter of the acquisition function.
+            acq_xi (float): Scaling parameter of the acquisition function.
+            mode (str): The mode that is used to evaluate the cost. Must be one of ``final`` or ``best``.
+            rerun_best_setting (bool): Whether to automatically rerun the best setting with 10 different seeds.
+            """
 
         self._set_seed(random_seed)
         log_path = os.path.join(output_dir, testproblem, self._optimizer_name)
@@ -100,9 +123,6 @@ class GP(Tuner):
         op = bayes_opt.BayesianOptimization(f = None, pbounds = self._bounds, random_state=random_seed)
         if kernel is not None:
             op._gp.kernel = kernel
-        if alpha is not None:
-            # set noise level
-            op._gp.alpha = alpha
 
         utility_func = UtilityFunction(acq_type, kappa = acq_kappa, xi = acq_xi)
 
