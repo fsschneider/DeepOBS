ca2fe38179d9221dc1c93334555a367ed1f1ea63
--global <frank.schneider@tuebingen.mpg.de>
2019-09-25 12:31:44 +0200

    Read full data set instead of just 64000 examples

--- deepobs/scripts/_svhn_preprocess.py
+++ deepobs/scripts/_svhn_preprocess.py
@@ -21,34 +21,34 @@ def preprocess(file_path="", save_path=""):
     train_file = os.path.join(file_path, "train_32x32.mat")
     read_input = scipy.io.loadmat(train_file)
     j = 0
-    output_file = open(save_path + '/data_batch_%d.bin' % j, 'ab')
+    output_file = open(save_path + "data_batch_%d.bin" % j, "ab")
 
-    for i in range(0, 64000):
+    for i in range(0, 73257):
 
         # create new bin file
         if i > 0 and i % 12800 == 0:
             output_file.close()
             j = j + 1
-            output_file = open(save_path + '/data_batch_%d.bin' % j, 'ab')
+            output_file = open(save_path + "data_batch_%d.bin" % j, "ab")
 
         # Write to bin file
-        if read_input['y'][i] == 10:
-            read_input['y'][i] = 0
-        read_input['y'][i].astype('uint8').tofile(output_file)
-        read_input['X'][:, :, :, i].astype('uint8').tofile(output_file)
+        if read_input["y"][i] == 10:
+            read_input["y"][i] = 0
+        read_input["y"][i].astype("uint8").tofile(output_file)
+        read_input["X"][:, :, :, i].astype("uint8").tofile(output_file)
 
     output_file.close()
 
     # Convert test images
     test_file = os.path.join(file_path, "test_32x32.mat")
     read_input = scipy.io.loadmat(test_file)
-    output_file = open(save_path + '/test_batch.bin', 'ab')
+    output_file = open(save_path + "test_batch.bin", "ab")
 
     for i in range(0, 26032):
         # Write to bin file
-        if read_input['y'][i] == 10:
-            read_input['y'][i] = 0
-        read_input['y'][i].astype('uint8').tofile(output_file)
-        read_input['X'][:, :, :, i].astype('uint8').tofile(output_file)
+        if read_input["y"][i] == 10:
+            read_input["y"][i] = 0
+        read_input["y"][i].astype("uint8").tofile(output_file)
+        read_input["X"][:, :, :, i].astype("uint8").tofile(output_file)
 
     output_file.close()
