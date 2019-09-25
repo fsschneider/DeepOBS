63353db722005957a6a255b485077483a842e109
Frank Schneider <frank.schneider@tuebingen.mpg.de>
2019-09-25 12:11:46 +0200

    Moved examples to separate folder

--- SGD.py
+++
@@ -1,5 +0,0 @@
-from deepobs.pytorch.runners.runner import StandardRunner
-from torch.optim.sgd import SGD
-hyperparams = {'lr': {'type': float}, 'momentum': {'type': float}, 'nesterov': {'type': bool}}
-runner = StandardRunner(SGD, hyperparams)
-runner.run()
