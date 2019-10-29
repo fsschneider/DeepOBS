b389a7c0a3129573d28308cd175002b612b016f6
Frank Schneider <frank.schneider@tuebingen.mpg.de>
2019-10-29 16:35:33 +0100

    Install specific version of matplotlib2tikz

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ import setuptools
 
 
 def readme():
-    with open('README.md') as f:
+    with open("README.md") as f:
         return f.read()
 
+
 setuptools.setup(
-    name='deepobs',
-    version='1.1.1',
-    description='Deep Learning Optimizer Benchmark Suite',
+    name="deepobs",
+    version="1.1.2",
+    description="Deep Learning Optimizer Benchmark Suite",
     long_description=readme(),
-    author='Frank Schneider, Lukas Balles and Philipp Hennig,',
-    author_email='frank.schneider@tue.mpg.de',
-    license='MIT',
+    author="Frank Schneider, Lukas Balles and Philipp Hennig,",
+    author_email="frank.schneider@tue.mpg.de",
+    license="MIT",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.6",
@@ -26,13 +27,18 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     install_requires=[
-        'argparse', 'numpy', 'pandas', 'matplotlib', 'matplotlib2tikz',
-        'seaborn'
+        "argparse",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "matplotlib2tikz==0.6.18",
+        "seaborn",
     ],
     scripts=[
-        'deepobs/scripts/deepobs_prepare_data.sh',
-        'deepobs/scripts/deepobs_get_baselines.sh',
-        'deepobs/scripts/deepobs_plot_results.py',
-        'deepobs/scripts/deepobs_estimate_runtime.py'
+        "deepobs/scripts/deepobs_prepare_data.sh",
+        "deepobs/scripts/deepobs_get_baselines.sh",
+        "deepobs/scripts/deepobs_plot_results.py",
+        "deepobs/scripts/deepobs_estimate_runtime.py",
     ],
-    zip_safe=False)
+    zip_safe=False,
+)
